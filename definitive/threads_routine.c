c34ca14a3e326a5453d21ba524b44d6f49d94633
Sergi <sergijp22@gmail.com>
2026-01-12 20:05:59 +0100

    threads_routine refactor

--- definitive/threads_routine.c
+++ definitive/threads_routine.c
@@ -16,6 +16,25 @@ static void *lonely_philo(p_philo *ph)
   return (NULL);
 }
 
+static void do_eat(p_philo *ph)
+{
+  safe_print(ph, "has taken a fork");
+  pthread_mutex_lock(&ph->ph_data_tx);
+  ph->eat_time = get_timestamp();
+  ph->num_eats++;
+  pthread_mutex_unlock(&ph->ph_data_tx);
+  safe_print(ph, "is eating");
+  usleep(ph->eat_ms * 1000);
+  pthread_mutex_unlock(ph->fork2);
+  pthread_mutex_unlock(ph->fork1);
+}
+
+static void do_sleep(p_philo *ph)
+{
+  safe_print(ph, "is sleeping");
+  usleep(ph->sleep_ms * 1000);
+}
+
 void *philo_routine(void *data)
 {
     p_philo *ph;
@@ -33,22 +52,13 @@ void *philo_routine(void *data)
               return (lonely_philo(ph));
             if (pthread_mutex_lock(ph->fork2) == 0)
             {
-                safe_print(ph, "has taken a fork");
-                pthread_mutex_lock(&ph->ph_data_tx);
-                ph->eat_time = get_timestamp();
-                ph->num_eats++;
-                pthread_mutex_unlock(&ph->ph_data_tx);
-                safe_print(ph, "is eating");
-                usleep(ph->eat_ms * 1000);
-                pthread_mutex_unlock(ph->fork2);
-                pthread_mutex_unlock(ph->fork1);
-                if (sim_finished(ph))
-                    return (NULL);
-                safe_print(ph, "is sleeping");
-                usleep(ph->sleep_ms * 1000);
-                if (sim_finished(ph))
-                    return (NULL);
-                safe_print(ph, "is thinking");
+              do_eat(ph);
+              if (sim_finished(ph))
+                return (NULL);
+              do_sleep(ph);
+              if (sim_finished(ph))
+                return (NULL);
+              safe_print(ph, "is thinking");
             }
         }
     }
