c1ee42b6f2624bda5a8026f0152b7940d80c79ef
Sergi <sergijp22@gmail.com>
2026-01-13 10:35:23 +0100

    Norminette

--- free_memory.c
+++ free_memory.c
@@ -1,57 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   free_memory.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: serez <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 10:14:33 by serez             #+#    #+#             */
+/*   Updated: 2026/01/13 10:33:58 by serez            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "philo.h"
 
-int free_all(t_ctx **ctx, pthread_mutex_t **forks, p_philo **ph)
+int	free_all(t_ctx **ctx, pthread_mutex_t **forks, p_philo **ph)
 {
-    (*ctx)->finish_simulation = 1;
-    free_forks(forks, (*ctx)->philos);
-    free_philos(ph, (*ctx)->philos, 0);
-    free_ctx(ctx);
-    return (1);
+	(*ctx)->finish_simulation = 1;
+	free_forks(forks, (*ctx)->philos);
+	free_philos(ph, (*ctx)->philos, 0);
+	free_ctx(ctx);
+	return (1);
 }
 
-int free_philos(p_philo **ph, int size, int mutex)
+int	free_philos(p_philo **ph, int size, int mutex)
 {
-    int i;
+	int	i;
 
-    if (!ph || !(*ph))
-        return (1);
-    i = -1;
-    while (++i < size)
-    {
-        if (mutex == 0)
-            pthread_mutex_destroy(&(*ph)[i].ph_data_tx);
-        else if (i != size - 1)
-            pthread_mutex_destroy(&(*ph)[i].ph_data_tx);
-    }
-    return (1);
+	if (!ph || !(*ph))
+		return (1);
+	i = -1;
+	while (++i < size)
+	{
+		if (mutex == 0)
+			pthread_mutex_destroy(&(*ph)[i].ph_data_tx);
+		else if (i != size - 1)
+			pthread_mutex_destroy(&(*ph)[i].ph_data_tx);
+	}
+	return (1);
 }
 
-int free_ctx(t_ctx **ctx)
+int	free_ctx(t_ctx **ctx)
 {
-    if (!ctx || !(*ctx))
-        return (1);
-    if ((*ctx)->write_init)
-        pthread_mutex_destroy(&(*ctx)->write);
-    if ((*ctx)->stop_lock_init)
-        pthread_mutex_destroy(&(*ctx)->stop_lock);
-    free(*ctx);
-    *ctx = NULL;
-    return (1);
+	if (!ctx || !(*ctx))
+		return (1);
+	if ((*ctx)->write_init)
+		pthread_mutex_destroy(&(*ctx)->write);
+	if ((*ctx)->stop_lock_init)
+		pthread_mutex_destroy(&(*ctx)->stop_lock);
+	free(*ctx);
+	*ctx = NULL;
+	return (1);
 }
 
-int free_forks(pthread_mutex_t **forks, int num)
+int	free_forks(pthread_mutex_t **forks, int num)
 {
-    int i;
+	int	i;
 
-    if (!forks ||!(*forks))
-        return (1);
-    i = 0;
-    while (i < num)
-    {
-        pthread_mutex_destroy(&((*forks)[i]));
-        i++;
-    }
-    free(*forks);
-    *forks = NULL;
-    return (1);
+	if (!forks ||!(*forks))
+		return (1);
+	i = 0;
+	while (i < num)
+	{
+		pthread_mutex_destroy(&((*forks)[i]));
+		i++;
+	}
+	free(*forks);
+	*forks = NULL;
+	return (1);
 }
