e151afee2a7a07fc9f30900be99ba6faf54035c8
Sergi <sergijp22@gmail.com>
2026-01-14 10:40:13 +0100

    safe_atoi

--- philo/ft_atoi.c
+++ philo/ft_atoi.c
@@ -6,79 +6,38 @@
 /*   By: sjuarez <sjuarez@student.42.barcelona>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 14:48:50 by sjuarez           #+#    #+#             */
-/*   Updated: 2026/01/13 10:34:24 by serez            ###   ########.fr       */
+/*   Updated: 2026/01/14 10:38:17 by serez            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <limits.h>
+
 #include "philo.h"
-
-static int	find_negative(const char *nptr)
-{
-	int	i;
-
-	i = 0;
-	while (nptr[i] != '\0' && !ft_isdigit(nptr[i]))
-	{
-		if (nptr[i] == '-')
-			return (-1);
-		i++;
-	}
-	return (1);
-}
-
-static int	find_first_num(const char *nptr)
-{
-	int	i;
-
-	i = 0;
-	while (nptr[i] != '\0')
-	{
-		if (ft_isdigit(nptr[i]))
-			return (i);
-		i++;
-	}
-	return (i);
-}
-
-static int	invalid_seq(const char *nptr, int limit)
-{
-	int	count;
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t'
-		|| nptr[i] == '\r' || nptr[i] == '\v' || nptr[i] == '\f')
-		i++;
-	while (nptr[i] != '\0' && i < limit)
-	{
-		if (!ft_isdigit(nptr[i]))
-			count++;
-		if (ft_isalpha(nptr[i]) || !ft_isprint(nptr[i]))
-			return (1);
-		i++;
-	}
-	return (count > 1);
-}
 
 int	ft_atoi(const char *nptr)
 {
-	int	result;
-	int	len;
+	long	result;
 	int	i;
+	int	negative;
 
-	i = 0 + find_first_num(nptr);
-	len = ft_strlen(nptr);
-	result = 0;
-	if (invalid_seq(nptr, i))
-		return (0);
-	while (i < len)
-	{
-		if (!ft_isdigit(nptr[i]))
-			break ;
-		result *= 10;
-		result += nptr[i] - '0';
+	i = 0;
+	while (nptr[i] && (nptr[i] >= '\t' && nptr[i] <= '\r'))
 		i++;
+	negative = 0;
+	if (nptr[i] && nptr[i] == '-')
+		negative = 1;
+	if (nptr[i] && (nptr[i] == '-' || nptr[i] == '+'))
+		i++;
+	if (nptr[i] == '\0')
+		return (-1);
+	result = 0;
+	while (nptr[i])
+	{
+		if (nptr[i] < '0' || nptr[i] > '9' || result < 0)
+			return (-1);
+		result = result * 10 + (nptr[i++] - '0');
+		if (result > (long)INT_MAX || (result > (long)INT_MIN && negative))
+			return (-1);
 	}
-	return (result * find_negative(nptr));
+	return (result * (negative * -2 + 1));
 }
